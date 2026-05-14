1b08a5836e701ffd445bd87edc6805b44df050ef
ronaldo isacc <u22208906@utp.edu.pe>
2026-05-14 14:43:18 -0500

    feat: correcciones

--- enjambre/src/main/java/com/redsolidaria/enjambre/model/CodigoVerificacion.java
+++ enjambre/src/main/java/com/redsolidaria/enjambre/model/CodigoVerificacion.java
@@ -20,6 +20,10 @@ public class CodigoVerificacion {
     private String codigo;
     private LocalDateTime fechaExpiracion;
     private boolean usado;
+    
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
 
     public CodigoVerificacion(String email, String codigo, LocalDateTime fechaExpiracion) {
         this.email = email;
